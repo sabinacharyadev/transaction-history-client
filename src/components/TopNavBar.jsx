cb7e76fc8e3a7f1fc25bd42a43f83d402e561b2a
sabinbabu <sabinacharya444@gmail.com>
2025-03-03 11:28:10 +1100

    logout feature implemented

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,16 +1,22 @@
 /* eslint-disable react/prop-types */
-import { Container, Navbar } from "react-bootstrap";
+import { Button, Container, Navbar, NavLink } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const TopNavbar = () => {
   const { user } = useSelector((state) => state.user);
+  const handleLogoutClick = () => {
+    window.location.reload();
+  };
   return (
     <Navbar className="bg-body-tertiary">
       <Container>
         <Navbar.Toggle />
 
-        <Navbar.Collapse className="justify-content-start">
+        <Navbar.Collapse className="justify-content-between">
           <Navbar.Text>Signed In As: {user.name}</Navbar.Text>
+          <NavLink onClick={handleLogoutClick} className="text-decoration-none">
+            <Button variant="outline-primary">Log out</Button>
+          </NavLink>
         </Navbar.Collapse>
       </Container>
     </Navbar>
